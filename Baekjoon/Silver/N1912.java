9dceb71fac9647a76a00e7fcac5232ebfd176cd9
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-01-14 22:48:00 +0900

    Create N1912.java

---
+++ Baekjoon/Silver/N1912.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int[] dp,arr;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		n = Integer.parseInt(br.readLine());
+		
+		dp = new int[n];
+		arr = new int[n];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int max = arr[0];
+		dp[0] = arr[0];
+		
+		for(int i=1;i<n;i++) {
+			dp[i] = Math.max(dp[i-1]+arr[i],arr[i]);
+			
+			max = Math.max(max,dp[i]);
+		}
+		
+		System.out.println(max);
+	}
+}
