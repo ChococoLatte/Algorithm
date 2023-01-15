204f10f072d7160e0ee01ec21e1e43b3f69acbe4
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-01-15 23:25:02 +0900

    Create N14002.java

---
+++ Baekjoon/Gold/N14002.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] arr,dp;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		N = Integer.parseInt(br.readLine());
+		
+		arr = new int[N];
+		dp = new int[N];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		dp[0]=1;
+		int max=1;
+		for(int i=1;i<N;i++) {
+			dp[i]=1;
+			for(int j=0;j<i;j++) {
+				if(arr[j]<arr[i] && dp[i]<=dp[j]) {
+					dp[i] = dp[j]+1;
+					max = Math.max(max, dp[i]);
+				}
+			}
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append(max).append("\n");
+		
+		Stack<Integer> stk = new Stack<>();
+		for(int i=N-1;i>=0;i--) {
+			if(dp[i]==max) {
+				stk.push(arr[i]);
+				max--;
+			}
+		}
+		while(!stk.isEmpty()) {
+			sb.append(stk.pop()+" ");
+		}
+		System.out.println(sb.toString());
+	}
+}
