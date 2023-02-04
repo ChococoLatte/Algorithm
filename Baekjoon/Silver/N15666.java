6d4cddc346cfa380b23d48f313df6e17df1560aa
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-02-04 23:58:03 +0900

    Create N15666.java

---
+++ Baekjoon/Silver/N15666.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static int[] ans,num;
+	static StringBuffer sb = new StringBuffer();
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		ans = new int[M];
+		num = new int[N+1];
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=1;i<=N;i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		
+		combination(1,0);
+		System.out.println(sb);
+	}
+	
+	private static void combination(int start, int cnt) {
+		
+		if(cnt==M) {
+			for(int i:ans) sb.append(i+" ");
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i=start;i<=N;i++) {
+			if(num[i-1]!=num[i]) {
+				ans[cnt] = num[i];
+				combination(i,cnt+1);
+			}
+		}
+	}
+}
