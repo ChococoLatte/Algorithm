a391369f29543f1088f718074a7d3e8d60fcae33
chococo_latte <62333600+ChococoLatte@users.noreply.github.com>
2023-02-04 23:59:12 +0900

    Create N15663.java

---
+++ Baekjoon/Silver/N15663.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static int[] ans,num;
+	static boolean[] isSelected;
+	static StringBuffer sb;
+	static LinkedHashSet<String> a;
+ 	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		ans = new int[M];
+		num = new int[N];
+		
+		isSelected = new boolean[N];
+		a = new LinkedHashSet<>();
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0;i<N;i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(num);
+		permutation(0);
+		
+		a.forEach(System.out::println);
+	}
+	
+	private static void permutation(int idx) {
+		
+		if(idx==M) {
+			sb = new StringBuffer();
+			for(int i: ans) sb.append(i+" ");
+			a.add(sb.toString());
+			return;
+		}
+		
+		for(int i=0;i<N;i++) {
+			if(isSelected[i]) continue;
+			isSelected[i]=true;
+			ans[idx]=num[i];
+			permutation(idx+1);
+			isSelected[i]=false;
+		}
+	}
+}
